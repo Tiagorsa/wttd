063536a95838c470eee91f304bc1a5c09686ce85
Tiago Sa <tiagosa@hotmail.com>
2019-01-13 01:31:52 -0300

    Corrige EMAIL paramets

--- eventex/settings.py
+++ eventex/settings.py
@@ -122,15 +122,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Email configuration
-
 EMAIL_BACKEND = config('EMAIL_BACKEND')
 EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_TLS = config('EMAIL_TLS', cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASWORD = config('EMAIL_HOST_PASWORD')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
