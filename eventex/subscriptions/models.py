977afaa51f8dc6e2e0c15613917ec7f7e807d27a
Tiago Sa <tiagosa@hotmail.com>
2019-01-14 01:45:01 -0300

    add paid field

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -8,6 +8,7 @@ class Subscription(models.Model):
     email = models.EmailField('e-amil')
     phone = models.CharField('Telefone', max_length=20)
     created_at = models.DateTimeField('Criado em', auto_now_add=True)
+    paid = models.BooleanField('pago', default=False)
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
 
     class Meta:
--- eventex/subscriptions/tests/test_model_subscribe.py
+++ eventex/subscriptions/tests/test_model_subscribe.py
@@ -21,3 +21,7 @@ class SubscriptionModelTest(TestCase):
 
     def teste_str(self):
         self.assertEqual('Tiago Sá', str(self.obj))
+
+    def test_paid_default_to_false(self):
+        """must default paid must be false"""
+        self.assertEqual(False, self.obj.paid)
