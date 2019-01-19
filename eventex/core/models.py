1879f68d9e202c7e95b53a215120cda72f7ae1b6
Tiago Sa <tiagosa@hotmail.com>
2019-01-19 10:34:25 -0300

    Contatos

--- eventex/core/models.py
+++ eventex/core/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.shortcuts import resolve_url as r
 
 
-
 class Speaker(models.Model):
     name = models.CharField('nome',  max_length=255)
     slug = models.SlugField('slug')
@@ -19,6 +18,28 @@ class Speaker(models.Model):
 
     def get_absolute_url(self):
         return r('speaker_detail', slug=self.slug)
+
+
+class Contact(models.Model):
+    EMAIL = 'E'
+    PHONE = 'P'
+    KINDS = (
+        (EMAIL, 'Email'),
+        (PHONE, 'Telefone'),
+    )
+    speaker = models.ForeignKey('Speaker', on_delete=models.CASCADE, verbose_name='palestrante')
+    kind = models.CharField('tipo', max_length=1, choices=KINDS)
+    value = models.CharField('valor', max_length=255)
+
+    def __str__(self):
+        return self.value
+
+    class Meta:
+        verbose_name = 'contato'
+        verbose_name_plural = 'contatos'
+
+
+
 
     #name = 'Grace Hopper'
     #website = 'http://hbn.link/hopper-site'
